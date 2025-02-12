01d562deff39e850261ffe9b4f2854278921ef11
mahat07 <140910546+mahat07@users.noreply.github.com>
2025-02-12 21:18:03 +0530

    Create  MyAbstractClass.java

---
+++ Day7/src/com.celcom.pakcage1/MyAbstractClass.java
@@ -0,0 +1,10 @@
+package com.celcom.package1;
+
+public abstract class MyAbstractClass {
+
+	public void myMethod1() {
+		System.out.println("Package 1 : My method 1");
+	}
+
+	abstract void myMethod2();
+}
